917201d3f90df392b88bbd84156bd8834f17e025
Jefferson Almeida <jeffersongoncalves473@gmail.com>
2020-08-18 20:47:17 -0300

    initial structure

---
+++ Apresentacao.REST.API/Controllers/JokesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Apresentacao.REST.API.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apresentacao.REST.API.Controllers
+{
+    [Route("joke")]
+    [ApiController]
+    public class JokesController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Get()
+        {
+            Random random = new Random();
+
+            List<Joke> jokes = new List<Joke>
+            {
+                new Joke("Piada 1"),
+                new Joke("Piada 2"),
+                new Joke("Piada 3"),
+                new Joke("Piada 4")
+            };
+
+            try
+            {
+                var response = jokes[random.Next(0, 3)];
+
+                return Ok(response);
+            }
+            catch(Exception ex)
+            {
+                var errorModel = new
+                {
+                    Mensagem = "Deu erro em algum trecho.",
+                    Erro = ex.StackTrace
+                };
+                
+                return StatusCode(500, errorModel);
+            }
+        }
+    }
+}
---
+++ Apresentacao.REST.API/Model/Joke.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Apresentacao.REST.API.Model
+{
+    public class Joke
+    {
+        public Joke(string value)
+        {
+            Value = value;
+            DiaGerado = DateTime.Now;
+        }
+
+        public string Value { get; set; }
+        public DateTime DiaGerado { get; set; }
+    }
+}
